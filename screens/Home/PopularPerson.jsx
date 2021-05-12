76f43dd71b19955feebded717257b691db280c25
Kwolik <kwolikzsp6@gmail.com>
2021-05-12 12:14:01 +0200

    Create component popular person

---
+++ screens/Home/PopularPerson.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import PeopleList from "../../components/PeopleList";
+
+export default function PopularPerson({navigation}) {
+  const [popularPerson, SetPopularPerson] = React.useState([]);
+
+  const getMovieRequest = async () => {
+    const url = `https://api.themoviedb.org/3/person/popular?api_key=730f5fc8cccd28b439fbcbac1988359b&language=en-US&page=1`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.results) {
+      SetPopularPerson(responseJson.results);
+    }
+  };
+
+  React.useEffect(() => {
+    getMovieRequest();
+  }, []);
+
+  const popularPersonList = popularPerson.map((people, index) => (
+    <PeopleList
+      key={index}
+      id={people.id}
+      name={people.name}
+      title={people.known_for[0].title}
+      // imdbid={people.imdbID}
+      poster={`https://image.tmdb.org/t/p/w342/${people.profile_path}`}
+      navigation={navigation}
+    />
+  ));
+
+  return <ScrollView horizontal={true}>{popularPersonList}</ScrollView>;
+}
