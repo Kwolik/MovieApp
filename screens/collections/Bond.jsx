e37ec8c33fefad1b75fb7c880cea9c84ed05daec
Kwolik <kwolikzsp6@gmail.com>
2021-05-12 12:11:28 +0200

    Create collection AHS and bond

---
+++ screens/collections/Bond.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import MovieList from "../../components/MovieList";
+
+export default function Bond({ navigation }) {
+  const [bond, SetBond] = React.useState([]);
+
+  const getMovieRequest = async () => {
+    const url = `https://api.themoviedb.org/3/collection/645?api_key=730f5fc8cccd28b439fbcbac1988359b&language=en-US`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.parts) {
+      SetBond(responseJson.parts);
+    }
+  };
+
+  React.useEffect(() => {
+    getMovieRequest();
+  }, []);
+
+  const bondList = bond.map((movie, index) => (
+    <MovieList
+      key={index}
+      id={movie.id}
+      title={movie.title}
+      year={movie.release_date ? movie.release_date : "20??"}
+      // imdbid={movie.imdbID}
+      type={"Movie"}
+      poster={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
+      navigation={navigation}
+      screen={1}
+    />
+  ));
+
+  return <ScrollView horizontal={true}>{bondList}</ScrollView>;
+}
